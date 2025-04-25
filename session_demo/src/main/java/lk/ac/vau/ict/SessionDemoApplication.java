ad4cb29eb56ad2c28a19f749935e3267cdb72175
Thilankavishka <thilankavishka25@gmail.com>
2025-04-25 16:35:56 +0530

    Project initialization

---
+++ session_demo/src/main/java/lk/ac/vau/ict/SessionDemoApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.vau.ict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionDemoApplication.class, args);
+	}
+
+}
---
+++ session_demo/src/test/java/lk/ac/vau/ict/SessionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.vau.ict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
